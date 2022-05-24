23cbb882432bb4dfde0aa053b20c3abe63ce5d42
Letjobana <suzanm@gerbertech.co.za>
2022-05-24 09:08:15 +0200

    Added a code to ask a user to enter their Id

---
+++ IdentityVerification/IdentityVerification/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IdentityVerification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string identity;
+            Console.WriteLine("Enter you Identity Number");
+            identity = Console.ReadLine();
+            Console.ReadLine();
+        }
+    }
+}
