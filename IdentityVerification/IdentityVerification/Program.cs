30dde04577b347a11d7497d35f67fd71f6619545
Letjobana <suzanm@gerbertech.co.za>
2022-05-24 09:14:26 +0200

    Added a code to validate the identity number

--- IdentityVerification/IdentityVerification/Program.cs
+++ IdentityVerification/IdentityVerification/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace IdentityVerification
 {
@@ -9,7 +10,15 @@ namespace IdentityVerification
             string identity;
             Console.WriteLine("Enter you Identity Number");
             identity = Console.ReadLine();
+            ValidateUserId(identity);
             Console.ReadLine();
+        }
+        public static void ValidateUserId(string userId)
+        {
+            if (userId.Length != 13 || Regex.IsMatch(userId, "[^0-9]"))
+                Console.WriteLine("Please enter a valid Id, A valid Id should contain 13 numbers");
+            else
+                Console.WriteLine("Thank you,your Id is Valid");
         }
     }
 }
