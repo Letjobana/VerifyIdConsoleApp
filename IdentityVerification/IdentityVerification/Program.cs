87e256f25cd3c5f0c4171d6fd18687829cec5601
Letjobana <suzanm@gerbertech.co.za>
2022-05-24 09:18:20 +0200

    Added a code to get date of birth of the user

--- IdentityVerification/IdentityVerification/Program.cs
+++ IdentityVerification/IdentityVerification/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Text.RegularExpressions;
 
 namespace IdentityVerification
@@ -19,6 +20,23 @@ namespace IdentityVerification
                 Console.WriteLine("Please enter a valid Id, A valid Id should contain 13 numbers");
             else
                 Console.WriteLine("Thank you,your Id is Valid");
+            GetDateOfBirth(userId);
+
+        }
+        public static void GetDateOfBirth(string birthDay)
+        {
+            DateTime dt;
+            var dateOfBirth = birthDay.Substring(0, 6);
+            if (DateTime.TryParseExact(dateOfBirth, "yyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None, out dt))
+            {
+                Console.WriteLine("Your birth date is:" + dt.ToString("yyyy/MM/dd", CultureInfo.InvariantCulture));
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid birth date");
+            }
+
         }
     }
 }
